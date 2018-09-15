9a2b303545e872ed92a69bd0545825deee851340
AnCorleone <anran0919@163.com>
2018-09-15 15:16:17 +0800

    使用Swagger2构建RESTful API文档

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# springboot
+# springboot 整合MyBatis RESTful API文档设计
+
+## 整合MyBatis
+- 在pom.xml中引入所需要的依赖
+- 使用MyBatis逆向工程
+    1. 先创建数据库 
+    2. 在application.yml中配置数据库连接池
+    3. 创建generatorConfig.xml 配置文件,并配置逆向工程生成文件的位置以及生成的表和类名
+    4. 在SpringBootDemoApplication中配置扫面的mapper包位置
+    5. 使用OverIsMergeablePlugin工具类(这里也可以使用插件,但是MyBatis plugin 需要付费),然后再com.zar.demo.generator.MybatisGenerator 运行MyBatis逆向工程生成类
+    6. 可以看到在对应文件夹中生成了*mapper.xml,*mapper.class,并且生成了对应的pojo类
+## 使用controller 返回json
+- 创建service
+    1. 在service包中创建接口,并创建impl包,在包中实现接口,在实现类上使用@servide注解,使用@Autowired 注解获取对应的*mapper对象,在实现方法中处理数据(这里的使用@Autowired 编辑器会报错,不用理会)
+- 创建controller
+    2. 在controller包中创建*Controller,在类上使用@RestController 注解, 创建方法配置请求的url和请求的类型
+## RESTful API文档设计
+- 在pom.xml中引入所需要的依赖
+- 
--- src/main/java/com/zar/demo/controller/CategoryController.java
+++ src/main/java/com/zar/demo/controller/CategoryController.java
@@ -4,10 +4,15 @@ import com.zar.demo.pojo.Category;
 import com.zar.demo.service.CategoryService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 
 /**
  * @author Anakin(Administrator)
@@ -15,27 +20,33 @@ import java.util.List;
  * 描述     ${TODO}
  */
 @RestController
+@RequestMapping("/category")
 class CategoryController {
 
     @Autowired
     CategoryService mCategoryService;
 
-    @RequestMapping("/list")
-    public List<Category> listCategory() {
-        System.out.println("##############");
+
+    @ApiOperation(value = "获取分类信息", notes = "获取分类信息")
+    @RequestMapping(value = {""}, method = RequestMethod.GET)
+    public List<Category> getCategoryList() {
         final List<Category> categories = mCategoryService.findAll();
-        System.out.println("##############  categories   " + categories);
         return categories;
     }
 
-    @RequestMapping("/get")
-    public Category get() {
-        final Category category = mCategoryService.get(1);
+
+    @ApiOperation(value = "获取用户详细信息", notes = "根据url的id来获取用户详细信息")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true,dataType = "int")
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Category getCategory(@PathVariable int id) {
+        final Category category = mCategoryService.get(id);
         return category;
     }
-
+/*
     @RequestMapping("/index")
     public String index() {
         return "index";
     }
+*/
+
 }
